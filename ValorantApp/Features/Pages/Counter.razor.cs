80e469db40c3964cb1dcf4a036f428eb83fce887
Ben Stroup <bstroup@iastate.edu>
2024-04-22 10:04:00 -0500

    git init

---
+++ ValorantApp/Features/Pages/Counter.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace ValorantApp.Features.Pages;
+
+public sealed partial class Counter
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
